af0f36bbd7db13f0002d71cf4b63f6761e6e9f1d
kenlhlui <116421546+kenlhlui@users.noreply.github.com>
2025-05-11 15:51:26 -0400

    Moved validation of basic (either -p or -d flag) para to cli_validation.py

--- dvmeta/cli_validation.py
+++ dvmeta/cli_validation.py
@@ -1,4 +1,6 @@
 """This module contains functions for validating command line arguments and environment variables."""
+import re
+
 from custom_logging import CustomLogger
 from typer import BadParameter
 
@@ -43,3 +45,18 @@ def validate_version_type(value: str) -> str:
         return value
     msg = f'Invalid value for --version: "{value}".\nMust be "draft", "latest", "latest-published", or a version number like "x" or "x.y".'  # noqa: E501
     raise BadParameter(msg)
+
+
+def validate_basic_input(dvdfds_matadata_option: bool, permission_option: bool) -> None:
+    """Validate the basic input options.
+
+    Args:
+        dvdfds_matadata_option (bool): Value of --dvdfds_metadata argument.
+        permission_option (bool): Value of --permission argument.
+
+    Raises:
+        BadParameter: If the combination of options is invalid.
+    """
+    if not dvdfds_matadata_option and not permission_option:
+        msg = 'Please provide the type of metadata to crawl. Use -d or/and -p flag for crawling of dataset metadata or permission metadata, respectively.'  # noqa: E501
+        raise BadParameter(msg)
